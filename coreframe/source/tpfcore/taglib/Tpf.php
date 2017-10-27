faf408c2f82d1e9a1012ffc91dad95df9606baec
tpframe <510974211@qq.com>
2017-10-27 14:49:18 +0800

    add tpf tag

--- coreframe/source/tpfcore/taglib/Tpf.php
+++ coreframe/source/tpfcore/taglib/Tpf.php
@@ -20,25 +20,28 @@ class Tpf extends Taglib
     // 标签定义
     protected $tags = [
         // 标签定义： attr 属性列表 close 是否闭合（0 或者1 默认1） alias 标签别名 level 嵌套层次  expression 是否允许表达式
-        'friendlink' => ['attr' => 'num,key,id'],
+        'friendlink' => ['attr' => 'num,key,id,order'],
     ];
     /**
      * 友情链接数据调用
      * num:获取记录数量
      * key:序号
      * id:循环中定义的元素变量
+     * sort:排序方式
      * {tpf:friendlink num='6'}{/tpf:friendlink} 
      */
     public function tagFriendlink($tag, $content){
         $id     = isset($tag['id'])?$tag['id']:'vo';
         $num    = isset($tag['num'])?(int)$tag['num']:1;
         $key    = isset($tag['key'])?$tag['key']:'key';
+        $order  = isset($tag['order'])?$tag['order']:'sort ASC';
         $parse  = '<?php ';
-        $parse .= '$tpfTagFriendlink =  Core::loadModel("Links")->getLinks(["limit"=>'.$num.']); ';
-        $parse .= 'foreach($tpfTagFriendlink as $'.$key.'=>$'.$id.'){';
+        $parse .= '$tpflist =  Core::loadModel("Links")->getLinks(["where"=>["status"=>1],"order"=>"'.$order.'","limit"=>'.$num.']); ';
+        $parse .= 'foreach($tpflist as $'.$key.'=>$'.$id.'){';
         $parse .= '?>';
         $parse .= $content;
         $parse .= '<?php } ?>';
+
         return $parse;
     }
 }
