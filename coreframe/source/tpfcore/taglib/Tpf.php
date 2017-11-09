f0d314ed0ffe27f49c803b332fa45f270c0cd699
tpframe <510974211@qq.com>
2017-11-09 14:26:19 +0800

    update

--- coreframe/source/tpfcore/taglib/Tpf.php
+++ coreframe/source/tpfcore/taglib/Tpf.php
@@ -20,7 +20,8 @@ class Tpf extends Taglib
     // 标签定义
     protected $tags = [
         // 标签定义： attr 属性列表 close 是否闭合（0 或者1 默认1） alias 标签别名 level 嵌套层次  expression 是否允许表达式
-        'friendlink' => ['attr' => 'num,key,id,order'],
+        'friendlink'    => ['attr' => 'num,key,id,order'],
+        'slide'         => ['attr' => 'num,key,id,order,where'],
     ];
     /**
      * 友情链接数据调用
@@ -37,6 +38,30 @@ class Tpf extends Taglib
         $order  = isset($tag['order'])?$tag['order']:'sort ASC';
         $parse  = '<?php ';
         $parse .= '$tpflist =  Core::loadModel("Links")->getLinks(["where"=>["status"=>1],"order"=>"'.$order.'","limit"=>'.$num.']); ';
+        $parse .= 'foreach($tpflist as $'.$key.'=>$'.$id.'){';
+        $parse .= '?>';
+        $parse .= $content;
+        $parse .= '<?php } ?>';
+
+        return $parse;
+    }
+    /**
+     * 幻灯片/广告位数据调用
+     * num:获取记录数量
+     * key:序号
+     * id:循环中定义的元素变量
+     * sort:排序方式
+     * where:查询条件 
+     * {tpf:slide num='1'}{/tpf:slide} 
+     */
+    public function tagSlide($tag, $content){
+        $id     = isset($tag['id']) && $tag['id']?$tag['id']:'vo';
+        $num    = isset($tag['num']) && $tag['num']?(int)$tag['num']:1;
+        $key    = isset($tag['key']) && $tag['key']?$tag['key']:'key';
+        $order  = isset($tag['order']) && $tag['order']?$tag['order']:'listorder ASC';
+        $where  = isset($tag['where']) && $tag['where']?$tag['where']:'1=1';
+        $parse  = '<?php ';
+        $parse .= '$tpflist =  Core::loadModel("Slide")->getTpfSlide(["where"=>"slide_status=1 AND '.$where.'","join"=>["join" => "__SLIDE_CAT__ b", "condition" => "__SLIDE__.slide_cid=b.cid", "type" => "INNER"],"order"=>"'.$order.'","field"=>"slide_id,slide_name,slide_pic,slide_url,slide_des,cat_name","limit"=>'.$num.']); ';
         $parse .= 'foreach($tpflist as $'.$key.'=>$'.$id.'){';
         $parse .= '?>';
         $parse .= $content;
