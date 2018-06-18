c8fdaffdea8fa224c8f6b4b2b2fee51b2400f9e3
510974211 <510974211@qq.com>
2018-06-18 18:37:56 +0800

    update 2.2.0618

--- application/common/controller/AddonAdminBase.php
+++ application/common/controller/AddonAdminBase.php
@@ -3,6 +3,7 @@
 // | Author: yaoyihong <510974211@qq.com>
 // +----------------------------------------------------------------------
 namespace app\common\controller;
+use tpfcore\Core;
 /**
  * 插件后台控制器基类
  */
@@ -16,13 +17,28 @@ class AddonAdminBase extends AddonBase
         // 执行父类构造方法
         parent::_initialize();
         $this->valildataAdmin();
+        $this->valildataPrivs();
     }
     /**
      * 验证用户
      */
     public function valildataAdmin(){
         if(!\think\Session::has("backend_author_sign")){
-            $this->redirect('backend/User/login');
+            $this->redirect(url('backend/User/login'));
+        }
+    }
+    /**
+     * 验证权限
+     */
+    public function valildataPrivs(){
+        $action=CONTROLLER_NAME.ACTION_NAME;
+        $menu=Core::loadModel("Menu","backend","logic")->getMenuArrTree(['display'=>1],true,true);
+        $privs=[];$outPrivs=["Indexmain","Indexindex"];
+        foreach ($menu as $key => $value) {
+            $privs[]=$value['controller'].strtolower($value['action']);
+        }
+        if(\think\Session::get("backend_author_sign")['userid']!=1 && !in_array($action, $outPrivs) && !in_array($action, array_unique($privs))){
+            $this->jump([RESULT_ERROR, '你没有权限进行此操作', null]);
         }
     }
 }
