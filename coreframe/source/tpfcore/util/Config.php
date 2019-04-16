d768da4aec468944e2e439c4aa52ba12f04d648f
tpframe@tom.com <tpframe@tom.com>
2019-04-16 11:11:03 +0800

    更新多维数组合并问题

--- coreframe/source/tpfcore/util/Config.php
+++ coreframe/source/tpfcore/util/Config.php
@@ -7,6 +7,9 @@
 namespace tpfcore\util;
 class Config{
 	
+	/* 
+		简单数组合并
+	*/
 	public static function updateConfig($file,$update_config){
 
 		$config=require($file);
@@ -16,6 +19,32 @@ class Config{
 		file_put_contents($file, "<?php \nreturn ".var_export($config,true).";");
 
 	}
+
+	/*
+		多维数组合并
+	*/
+	public static function mult_array_merge($file,$update_config){
+
+		$config = require($file);
+
+		Config::array_merge($config,$update_config);
+
+		file_put_contents($file, "<?php \nreturn ".var_export($config,true).";");
+	}
+
+	
+	private static function array_merge(&$old_arr,$new_arr)
+	{
+	    foreach ($new_arr as $key => $value) {
+	        if(is_array($value)){
+	            Config::array_merge($old_arr[$key],$value);
+	        }else{
+	            $old_arr[$key]=$value;
+	        }
+	    }
+	    return $old_arr;
+	}
+
 	/*
 		返回某个数组键的值，支持aa.bb.cc
 	*/
